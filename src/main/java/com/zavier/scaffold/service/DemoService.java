1f5409b698b0d8c5ba2cc9c9b4fd046536866e03
zheng <765324639@qq.com>
2018-05-27 12:45:00 +0800

    打印MyBatis SQL日志，以及添加对事物回滚的测试

--- src/main/java/com/zavier/scaffold/service/DemoService.java
+++ src/main/java/com/zavier/scaffold/service/DemoService.java
@@ -1,10 +1,35 @@
 package com.zavier.scaffold.service;
 
+import com.zavier.scaffold.dao.CustomerMapper;
+import com.zavier.scaffold.pojo.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DemoService {
+    @Autowired
+    private CustomerMapper customerMapper;
+
     public String test() {
         return "DemoService";
+    }
+
+    @Transactional
+    public void testRollBack() {
+        Customer customer = new Customer();
+        customer.setName("roll1");
+        customerMapper.insertSelective(customer);
+        Customer customer1 = new Customer();
+        customer1.setName("testrollbackdadfa2");
+        customerMapper.insertSelective(customer1);
+    }
+
+    @Transactional
+    public void testRollBack1() {
+        Customer customer = new Customer();
+        customer.setName("roll1");
+        customerMapper.insertSelective(customer);
+        throw new RuntimeException("就是不让你提交，看事物好用不");
     }
 }
--- src/main/java/com/zavier/scaffold/web/DemoController.java
+++ src/main/java/com/zavier/scaffold/web/DemoController.java
@@ -2,7 +2,9 @@ package com.zavier.scaffold.web;
 
 import com.zavier.scaffold.common.ProcessException;
 import com.zavier.scaffold.common.ResultBean;
+import com.zavier.scaffold.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ public class DemoController {
 
     @Value("${amazon.sourceId}")
     private String sourceId;
+
+    @Autowired
+    private DemoService demoService;
 
     @GetMapping("/test")
     public ResultBean<String> test() {
@@ -30,5 +35,17 @@ public class DemoController {
     public String testError1() {
         log.error("error");
         throw new ProcessException("testError");
+    }
+
+    @GetMapping("/rollback")
+    public ResultBean<String> testRollBack() {
+        demoService.testRollBack();
+        return ResultBean.createBySuccess();
+    }
+
+    @GetMapping("/rollback1")
+    public ResultBean<String> testRollBack1() {
+        demoService.testRollBack1();
+        return ResultBean.createBySuccess();
     }
 }
